175269656ca899c71ac334e17f5f92d9a6a0b0b4
Sylvain T <sylvaintagnabou@gmail.com>
2023-09-27 15:02:42 +0000

    task01

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,11 @@
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task = true; // 'task' is assigned a value but never used   no-unused-vars
+    const task2 = false; // 'task' is assigned a value but never used   no-unused-vars
+  }
+
+  return [task, task2];
+}
