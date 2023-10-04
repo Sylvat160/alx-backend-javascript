f89548eb4a7ea286e0e41fe6da9e9b7231fc06e1
Sylvain T <sylvaintagnabou@gmail.com>
2023-10-04 21:26:03 +0000

    ...

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,22 @@
+export interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
+
+export interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+export interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+export function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`;
+}
+
+
